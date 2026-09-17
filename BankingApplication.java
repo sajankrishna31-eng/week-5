00e62a1d0ab83514ee805342273d12a1148e686b
sajankrishna31-eng <sajankrishna31@gmail.com>
2026-09-17 13:27:45 +0530

    Add Tutorial 6 Q3 Advanced Security interfaces

---
+++ BankingApplication.java
@@ -0,0 +1,35 @@
+public class BankingApplication implements AdvancedSecurity {
+    public void login() {
+        System.out.println("Login successful");
+    }
+
+    public void logout() {
+        System.out.println("Logout successful");
+    }
+
+    public void fingerprintAuthentication() {
+        System.out.println("Fingerprint authentication successful");
+    }
+
+    public void faceRecognition() {
+        System.out.println("Face recognition authentication successful");
+    }
+
+    public static void main(String[] args) {
+        BankingApplication app = new BankingApplication();
+        app.login();
+        app.fingerprintAuthentication();
+        app.faceRecognition();
+        app.logout();
+    }
+}
+
+interface BasicSecurity {
+    void login();
+    void logout();
+}
+
+interface AdvancedSecurity extends BasicSecurity {
+    void fingerprintAuthentication();
+    void faceRecognition();
+}
